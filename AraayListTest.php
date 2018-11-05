23b877b7883b449eae4baa3cad88c40e2a4844cb
Alexnguyen17 <Alexnguyen170395@gmail.com>
2018-11-05 08:33:22 +0700

    file

---
+++ AraayListTest.php
@@ -0,0 +1,17 @@
+<?php
+
+include "ArrayList.php";
+
+$listInteger = new ArrayList();
+$listInteger->add(1);
+$listInteger->add(2);
+$listInteger->add(3);
+
+echo $listInteger->get(6);
+
+/**
+ * Created by PhpStorm.
+ * User: alexnguyen
+ * Date: 05/11/2018
+ * Time: 08:16
+ */
